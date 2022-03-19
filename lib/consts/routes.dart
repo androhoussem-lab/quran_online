ca86fe803eb25bad4deea20af2a1a52eafdc6e37
houssemeddine <houssem.macbook@gmail.com>
2022-03-19 03:20:04 +0100

    edit main add pages from independent file

---
+++ lib/consts/routes.dart
@@ -0,0 +1,56 @@
+import 'package:get/get.dart';
+
+import '../views/pages/authentication/authentication_page.dart';
+import '../views/pages/authentication/login_page.dart';
+import '../views/pages/authentication/payment_page.dart';
+import '../views/pages/course_content_page.dart';
+import '../views/pages/course_detail_page.dart';
+import '../views/pages/courses_page.dart';
+import '../views/pages/video_page.dart';
+
+
+List<GetPage> pages = [
+
+    GetPage(
+      name: '/authentication_page',
+      page: () => const AuthenticationPage(),
+      transition: Transition.rightToLeftWithFade,
+      transitionDuration: const Duration(milliseconds: 600),
+    ),
+    GetPage(
+      name: '/login_page',
+      page: () => LoginPage(),
+      transition: Transition.rightToLeftWithFade,
+      transitionDuration: const Duration(milliseconds: 600),
+    ),
+    GetPage(
+      name: '/payment_page',
+      page: () => PaymentPage(),
+      transition: Transition.rightToLeftWithFade,
+      transitionDuration: const Duration(milliseconds: 600),
+    ),
+    GetPage(
+      name: '/home',
+      page: () => CoursePage(),
+      transition: Transition.rightToLeftWithFade,
+      transitionDuration: const Duration(milliseconds: 600),
+    ),
+    GetPage(
+      name: '/course_detail_page',
+      page: () => CourseDetailPage(),
+      transition: Transition.rightToLeftWithFade,
+      transitionDuration: const Duration(milliseconds: 600),
+    ),
+    GetPage(
+      name: '/course_content_page',
+      page: () => CourseContentPage(),
+      transition: Transition.rightToLeftWithFade,
+      transitionDuration: const Duration(milliseconds: 600),
+    ),
+    GetPage(
+      name: '/video_page',
+      page: () => VideoPage(),
+      transition: Transition.rightToLeftWithFade,
+      transitionDuration: const Duration(milliseconds: 600),
+    ),
+  ];
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:quran_online/consts/routes.dart';
 import 'package:quran_online/database/database.dart';
 import 'package:quran_online/utils/initial_binding.dart';
 import 'package:quran_online/utils/custom_theme.dart';
 import 'package:quran_online/views/pages/authentication/authentication_page.dart';
-import 'package:quran_online/views/pages/authentication/login_page.dart';
-import 'package:quran_online/views/pages/authentication/payment_page.dart';
-import 'package:quran_online/views/pages/course_content_page.dart';
-import 'package:quran_online/views/pages/course_detail_page.dart';
-import 'package:quran_online/views/pages/courses_page.dart';
-import 'package:quran_online/views/pages/video_page.dart';
+
 
 void main() async{
   await DataBase.init();
@@ -26,50 +22,7 @@ class MyApp extends StatelessWidget {
       locale: const Locale('ar', 'dz'),
       theme: CustomTheme.myTheme,
       initialBinding: InitialBinding(),
-      getPages: [
-        GetPage(
-          name: '/authentication_page',
-          page: () => const AuthenticationPage(),
-          transition: Transition.rightToLeftWithFade,
-          transitionDuration: const Duration(milliseconds: 600),
-        ),
-        GetPage(
-          name: '/login_page',
-          page: () => LoginPage(),
-          transition: Transition.rightToLeftWithFade,
-          transitionDuration: const Duration(milliseconds: 600),
-        ),
-        GetPage(
-          name: '/payment_page',
-          page: () => PaymentPage(),
-          transition: Transition.rightToLeftWithFade,
-          transitionDuration: const Duration(milliseconds: 600),
-        ),
-        GetPage(
-          name: '/home',
-          page: () => CoursePage(),
-          transition: Transition.rightToLeftWithFade,
-          transitionDuration: const Duration(milliseconds: 600),
-        ),
-        GetPage(
-          name: '/course_detail_page',
-          page: () => CourseDetailPage(),
-          transition: Transition.rightToLeftWithFade,
-          transitionDuration: const Duration(milliseconds: 600),
-        ),
-        GetPage(
-          name: '/course_content_page',
-          page: () => CourseContentPage(),
-          transition: Transition.rightToLeftWithFade,
-          transitionDuration: const Duration(milliseconds: 600),
-        ),
-        GetPage(
-          name: '/video_page',
-          page: () => VideoPage(),
-          transition: Transition.rightToLeftWithFade,
-          transitionDuration: const Duration(milliseconds: 600),
-        ),
-      ],
+      getPages: pages,
       home: const AuthenticationPage(),
     );
   }
